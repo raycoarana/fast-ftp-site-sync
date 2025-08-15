2c5d07c7f9156e596084c8eb0ab3d54fd52f085a
rayco <me@raycoarana.com>
2025-08-15 02:20:41 +0200

    Clean-up

--- test/run-local-test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-echo "🧪 Local Action Test"
-echo "� Running unified integration tests..."
-
-# Run the unified integration test script in local mode
-$(dirname "$0")/integration-test.sh local
